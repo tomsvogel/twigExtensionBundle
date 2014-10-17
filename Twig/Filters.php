9109b78f00aef0659bbadce037480eeadbc27f78
Thomas Vogel <toms.vogel@gmail.com>
2014-10-17 03:25:51 +0200

    finished 2 Filters! sortArray('date') removePast('date')

---
+++ Twig/Filters.php
@@ -0,0 +1,52 @@
+<?php
+namespace Arkulpa\Bundle\TwigExtensionBundle\Twig;
+
+
+use Doctrine\Tests\DBAL\Types\DateTest;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+class Filters extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter(
+                'sortArray', array($this, 'sortArrayFnc')
+            ),
+            new \Twig_SimpleFilter(
+                'removePast', array($this, 'removePastFnc')
+            ),
+        );
+    }
+
+    function sortArrayFnc($arr, $col, $dir = SORT_ASC)
+    {
+        $sort_col = array();
+        foreach ($arr as $key => $row) {
+            $sort_col[$key] = $row[$col];
+        }
+        array_multisort($sort_col, $dir, $arr);
+
+        return $arr;
+    }
+
+    function removePastFnc($arr, $col)
+    {
+        $result = array();
+        $d = new \DateTime();
+        $dstr = $d->format('Y-m-d');
+        foreach ($arr as $a) {
+            if ($a[$col] > $dstr) {
+                $result[] = $a;
+            }
+        }
+
+        return $result;
+    }
+
+
+    public function getName()
+    {
+        return 'arkulpa_filter';
+    }
+}
